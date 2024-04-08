1cb65a8e76c3a2a9b6657fca1691f2c1d65acda3
bleepbloopbeert <114595086+coolmankijhl@users.noreply.github.com>
2024-04-07 19:10:40 -0500

    merged bookshelf.c and bookshelf.h, organized functions for maintainability

--- bookshelf.c
+++ bookshelf.c
@@ -3,8 +3,42 @@
 #include <string.h>
 
 #include "stack.h"
-#include "bookshelf.h"
 #include "global.h"
+
+#define SHELVES 5
+
+char userArgs[SHELVES][100];
+struct stackNode* shelves[SHELVES];
+
+struct command {
+	char *cmd;
+	void (*function)();
+}; 
+
+// First argument conversions
+struct command conversions1[] = {
+	{"exit", exitCMD},
+	{"help", help},
+	{"create", create},
+	{"clear", clear},
+	{"show", show},
+	{"rm", rm},
+	{NULL, NULL}
+};
+
+// Conversions for create argument
+struct command createConvert[] = {
+	{"shelf", createShelf},
+	{"book", createBook},
+	{NULL, NULL}
+};
+
+// Conversions for show argument
+struct command showConvert[] = {
+	{"shelf", showShelf},
+	{"shelves", showShelves},
+	{NULL, NULL}
+};
 
 // Parses input for the shell
 int main()
@@ -36,6 +70,63 @@ int main()
 			
 	}
 		
+}
+
+// Calls conversion maps
+void executeCommand(char* cmdName, struct command* convertLayer)
+{
+	for (struct command* entry = convertLayer; entry->cmd != NULL; entry++)
+       	{
+		if (strcmp(entry->cmd, cmdName) == 0)
+	       	{
+			entry->function();
+			return;
+		}
+	}
+	printf("Command not recognized, type \"help\" for a list of commands.\n");
+}
+
+// Calls create conversion map
+void create()
+{
+	executeCommand(userArgs[1], createConvert);
+}
+
+// Calls addBook
+void createBook()
+{
+	addBook(userArgs[2], userArgs[3], &shelves[atoi(userArgs[4])-1]);
+}
+
+// Adds book in a shelf
+void addBook(char bookName[], char author[], stackNode** top)
+{
+	if(bookName[0] == '\0' || bookName[0] == '\0')
+	{
+		printf("ERROR create book: no author or name arg\n");
+		return;
+	}	
+	if(*top == NULL)
+	{
+		printf("ERROR create book: no shelf arg or shelf is doesn't exist\n");
+		return;
+	}
+	push(top);
+
+	(*top)->bookName = (char*)malloc(sizeof(char) * (strlen(bookName) + 1));
+	(*top)->author = (char*)malloc(sizeof(char) * (strlen(author) + 1));
+
+	strcpy((*top)->bookName, bookName);
+	strcpy((*top)->author, author);
+
+	if(*top != NULL)
+		printf("Successfully created book %s by %s\n", (*top)->bookName, (*top)->author);
+}
+
+// Calls addShelf
+void createShelf()
+{
+	addShelf(userArgs[2], userArgs[3], atoi(userArgs[4])-1);	
 }
 
 // Creates a shelf in a shelf slot
@@ -81,32 +172,33 @@ void addShelf(char shelfName[], char username[], int n)
 		printf("ERROR create shelf: failed creating shelf\n");
 }
 
-// Adds book in a shelf
-void addBook(char bookName[], char author[], stackNode** top)
+// Calls show conversion map
+void show()
 {
-	if(bookName[0] == '\0' || bookName[0] == '\0')
-	{
-		printf("ERROR create book: no author or name arg\n");
-		return;
-	}	
-	if(*top == NULL)
-	{
-		printf("ERROR create book: no shelf arg or shelf is doesn't exist\n");
-		return;
-	}
-	push(top);
-
-	(*top)->bookName = (char*)malloc(sizeof(char) * (strlen(bookName) + 1));
-	(*top)->author = (char*)malloc(sizeof(char) * (strlen(author) + 1));
-
-	strcpy((*top)->bookName, bookName);
-	strcpy((*top)->author, author);
-
-	if(*top != NULL)
-		printf("Successfully created book %s by %s\n", (*top)->bookName, (*top)->author);
+	executeCommand(userArgs[1], showConvert);
 }
 
-// Displays shelf head and its following books
+// Prints header information for all shelves
+void showShelves()
+{
+	for(int i = 0; i < SHELVES; i++)
+	{
+		if(shelves[i] != NULL)
+			printf("Shelf %d: %s by %s\n", i+1, shelves[i]->bookName, shelves[i]->author);
+		else
+			printf("Shelf %d: SHELF SLOT EMPTY\n", i+1);
+	}
+}
+
+// Calls displayShelf if arg is [1, SHELVES] inclusive
+void showShelf()
+{
+	int n = atoi(userArgs[2]);
+	if(n < SHELVES+1 && n > 0)
+		displayShelf(shelves[n-1], n);
+}
+
+// Displays shelf head, its following books, and their respective information
 void displayShelf(stackNode *top, int n)
 {
 	if(top == NULL)
@@ -125,6 +217,12 @@ void displayShelf(stackNode *top, int n)
 	free(ptr);
 }
 
+// Calls emptyShelf for a shelf slot
+void rm()
+{
+	emptyShelf(atoi(userArgs[1]));	
+}
+
 // Destroys shelf and frees memory from it and its books
 void emptyShelf(int n)
 {
@@ -138,4 +236,42 @@ void emptyShelf(int n)
 	}
 	destroy(shelves[n-1]);
 	shelves[n-1] = NULL;
+}
+
+// Displays help information from commandHelp.txt
+void help()
+{
+	FILE *file = fopen("commandHelp.txt", "r");
+	if(file == NULL)
+	{
+		printf("ERROR commandHelp.txt file not found.\n");
+		return;
+	}
+
+	system("clear");
+
+	char c  = fgetc(file);
+	while(c != EOF)
+	{
+		printf("%c", c);
+		c = fgetc(file);
+	}
+	fclose(file);
+}
+
+// exits bookshell.c
+void exitCMD()
+{
+	printf("Exiting..\n");
+
+	for(int i = 0; i < SHELVES; i++)
+			emptyShelf(i);
+
+	exit(1);  
+}
+
+// Clears screen
+void clear()
+{
+	system("clear");
 }
